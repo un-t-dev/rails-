0be38c6e558b9313e74e5978527ef2b1da9a0048
EC2 Default User <ec2-user@ip-172-31-73-129.ec2.internal>
2022-05-21 10:17:51 +0000

    commit

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,13 +2,16 @@ class ReservationsController < ApplicationController
     before_action :authenticate_user!
     
     def new
+      binding.pry
       @user = current_user.id
       @room = Room.find(params[:room_id])
       @reservation = Reservation.new(params.permit(:room_id, :start_date, :end_date, :person, :total))
       @reservation.user_id = current_user.id
       @reservation.person = person.to_i
-      @days = (@reservation.end_date - @reservation.start_date).to_i
-      @reservation.total = @days * @reservation.person * @room.price
+      if @reservation.valid?
+       @days = (@reservation.end_date - @reservation.start_date).to_i
+       @reservation.total = @days * @reservation.person * @room.price
+      end
     end
     
     
