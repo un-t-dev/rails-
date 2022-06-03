e1bcc7ce87ee2f28b0a8ad91d2506586c8c72f15
EC2 Default User <ec2-user@ip-172-31-73-129.ec2.internal>
2022-06-03 12:25:10 +0000

    エラーに関して

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,29 +2,18 @@ class ReservationsController < ApplicationController
     before_action :authenticate_user!
     
     def new
-      @user = current_user.id
-      @room = Room.find(params[:room_id])
-      @reservation = Reservation.new(params.permit(:room_id, :start_date, :end_date, :person, :total))
-      @reservation.user_id = current_user.id
-      if @reservation.valid?
-       @days = (@reservation.end_date - @reservation.start_date).to_i
-       @reservation.total = @days * @reservation.person * @room.price
-       binding.pry
-      end
+      @reservation = Reservation.new(reservation_params)
+      @days = (@reservation.end_date - @reservation.start_date).to_i
     end
     
     
     def create
-      @reservation = Reservation.new(params.permit(:room_id, :start_date, :end_date, :person, :total, :user_id))
-      @room = Room.find(params[:reservation][:room_id])
-      if @reservation.save
-      end
     end
     
     
     
     private
       def reservation_params
-        params.require(:reservation).permit(:start_date, :end_date)
+        params.require(:reservation).permit(:start_date, :end_date, :person).merge(user_id: current_user.id, room_id: params[:reservation][:room_id])
       end
 end
